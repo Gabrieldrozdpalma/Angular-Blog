3b306914e371a66139156738ff929726242bd05b
gabriel <gabrield.palma@gmail.com>
2023-05-12 17:13:48 -0300

    content created

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Conferência trouxe novidades, confira algumas:",
+        "description": "Entre as novidades são esperados o novo Android 14, Google Pixel 7a e o Pixel Fold.",
+        "photoCover": "https://t2.tudocdn.net/668402?w=1920"
+    },
+    {
+        "id":"2",
+        "title": "Drama de família? Business? Reduzir Sucession a isso é apequenar a série",
+        "description": "ola",
+        "photoCover": "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhKxRyseLRvaSmCxw08xjPrEC2cRwGn0iX76Z5j9VuApKLe8M0rVV4KfW3TOrCwr0xg8KlI3SJJuVbkVjTstnH7UMJsf3bYSkwxh7n_Im7EWtm6wo3gikzbPjALd34OVS-kgKHVUvDHjBu4DvmLZxnxYv4jZc6mZCtP54FAxIhhBq64JVR7HvkKrOVVrw/s620/E%207.jpg"
+    }
+]
